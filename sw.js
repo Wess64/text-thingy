78ab0ab7a41b75dbeba8479603889a725cef8568
Wess64 <166668969+Wess64@users.noreply.github.com>
2025-02-16 20:20:22 -0300

    making this an app #3

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'text-thingy-v1';
+     const ASSETS = [
+       '/text-thingy/',
+       '/text-thingy/index.html',
+       '/text-thingy/script.js',
+       '/text-thingy/styles.css',
+       '/text-thingy/icon-192x192.png',
+       '/text-thingy/icon-512x512.png'
+     ];
+
+     self.addEventListener('install', (event) => {
+       event.waitUntil(
+         caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+       );
+     });
+
+     self.addEventListener('fetch', (event) => {
+       event.respondWith(
+         caches.match(event.request).then((response) => response || fetch(event.request))
+       );
+     });
