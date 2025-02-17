4e93dc73f6c886909ac87c98cefcead085760b49
Wess64 <166668969+Wess64@users.noreply.github.com>
2025-02-16 21:31:23 -0300

    updating even more 1

--- sw.js
+++ sw.js
@@ -1,20 +1,20 @@
-const CACHE_NAME = 'text-thingy-v1';
-     const ASSETS = [
-       '/text-thingy/',
-       '/text-thingy/index.html',
-       '/text-thingy/script.js',
-       '/text-thingy/icon-192x192.png',
-       '/text-thingy/icon-512x512.png'
-     ];
+const CACHE_NAME = 'text-thingy-v2'; // Update the version number
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/script.js',
+  '/icon-192x192.png',
+  '/icon-512x512.png',
+];
 
-     self.addEventListener('install', (event) => {
-       event.waitUntil(
-         caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-       );
-     });
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
 
-     self.addEventListener('fetch', (event) => {
-       event.respondWith(
-         caches.match(event.request).then((response) => response || fetch(event.request))
-       );
-     });
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request)
+  );
+});
